8c982afc47f53a53da6a11bd99da20481f4d74d3
Alexandros5880 <Alexpl_15@gmail.com>
2022-02-02 17:00:00 +0200

    Update all Fonts Families.

---
+++ MyWebSite/wwwroot/js/HomeIndex.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    $(".link").hover(function () {
+        var polygon = $(this).children().children().children();
+        var text = polygon.children();
+        polygon.attr("fill-opacity", "0.4");
+    }, function () {
+        var polygon = $(this).children().children().children();
+        var text = polygon.children();
+        polygon.attr("fill-opacity", "1");
+    });
+
+});
---
+++ MyWebSite/wwwroot/js/_NavBar.js
@@ -0,0 +1,30 @@
+﻿
+
+
+
+$(document).ready(function () {
+
+    
+    $(".nav-item").on("click", function () {
+        //$("a").find(".active").removeClass("active");
+        //$(this).children().addClass("active");
+        //$(this).children().css("color", "green");
+    });
+
+
+    $("#down").css("display", "block");
+    $("#up").css("display", "none");
+    $(".navbar-toggler").click(function () {
+        if ($('#navbarSupportedContent').is(':visible')) {
+            $("#down").css("display", "block");
+            $("#up").css("display", "none");
+        } else {
+            $("#down").css("display", "none");
+            $("#up").css("display", "block");
+        }
+    });
+
+
+});
+
+
